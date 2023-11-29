0f492e3802c0ba2e20c19617886a206b9600c2b8
ghouili <walid.ghouili98@gmail.com>
2023-11-29 18:20:01 +0100

    seance 03

--- src/cotrollers/user.js
+++ src/cotrollers/user.js
@@ -2,8 +2,59 @@ const user = require('../models/user');
 
 const bcrypt = require('bcryptjs');
 
-const AddUser = async(req, res) => {
-    return res.send('user is added in user.controller!!!');
+
+const GetAllUser = async(req, res) => {
+
+    let existingusers;
+    try {
+        existingusers = await user.find();
+    } catch (error) {
+        return res.status(500).json({success: false, message: "server error while fetching user", data: error});
+    }
+    
+    return res.status(200).json({success: true, message: `All Users`, data: existingusers});
+}
+
+const FindUserById = async(req, res) => {
+
+    const {id} = req.params;
+
+    let existinguser;
+    try {
+        existinguser = await user.findById(id);
+    } catch (error) {
+        return res.status(500).json({success: false, message: "server error while fetching user", data: error});
+    }
+
+    if (!existinguser) {
+        return res.status(400).json({success: false, message: "User doesn't exist!! ", datat: null});
+    }  
+    
+    return res.status(200).json({success: true, message: `User was found`, data: existinguser});
+}
+
+const DeleteUser = async(req, res) => {
+
+    const {id} = req.params;
+
+    let existinguser;
+    try {
+        existinguser = await user.findById(id);
+    } catch (error) {
+        return res.status(500).json({success: false, message: "server error while fetching user", data: error});
+    }
+
+    if (!existinguser) {
+        return res.status(400).json({success: false, message: "User doesn't exist!! ", datat: null});
+    }  
+
+    try {
+        await existinguser.deleteOne();
+    } catch (error) {
+        return res.status(500).json({success: false, message: "server error while saving user", data: error});
+    }
+    
+    return res.status(200).json({success: true, message: `User was Deleted successfully`, data: null});
 }
 
 const Register = async(req, res) => {
@@ -64,6 +115,40 @@ const Login = async(req, res) => {
 
 }
 
-exports.AddUser = AddUser
+const UpdateUser = async(req, res) => {
+
+    const {email, name, password, phone} = req.body;
+    const {id} = req.params;
+
+    let existinguser;
+    try {
+        existinguser = await user.findById(id);
+    } catch (error) {
+        return res.status(500).json({success: false, message: "server error while fetching user", data: error});
+    }
+
+    if (!existinguser) {
+        return res.status(400).json({success: false, message: "user doesn't exist!! ", datat: null});
+    }  
+
+    existinguser.email = email
+    existinguser.phone = phone
+    existinguser.name = name
+    existinguser.password = await bcrypt.hash(password, 10);
+
+    try {
+        await existinguser.save();
+    } catch (error) {
+        return res.status(500).json({success: false, message: "server error while saving user", data: error});
+    }
+
+    return res.status(200).json({success: true, message: `user Updated successfly`, data: existinguser});
+
+}
+
+exports.GetAllUser = GetAllUser
+exports.FindUserById = FindUserById
+exports.DeleteUser = DeleteUser
 exports.Login = Login
 exports.Register = Register
+exports.UpdateUser = UpdateUser
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,14 @@ const express = require('express');
 const UserController = require('../cotrollers/user');
 const route = express.Router();
 
-route.get('/add', UserController.AddUser);
+
+route.get('/', UserController.GetAllUser);
+
+route.get('/:id', UserController.FindUserById);
+
+route.patch('/:id', UserController.UpdateUser);
+
+route.delete('/:id', UserController.DeleteUser);
 
 route.post('/register', UserController.Register);
 
